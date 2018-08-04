fbd09340cb8e0a66a1c297667109968d5bea0842
joshua.nicholl@griffithuni.edu.au <joshua.nicholl@griffithuni.edu.au>
2018-08-05 02:48:51 +1000

    Readd functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { Announcement } from '../../models/Announcement';
+
+import { DateProvider } from '../../providers/date/date';
+import { AnnouncementsProvider } from '../../providers/announcements/announcements';
+import { UserProvider } from '../../providers/user/user';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  styleUrls: ['home.scss']
 })
 export class HomePage {
-  pageTitle: string = 'Sign In';
-  email:string;
-  password:string;
-  constructor(public navCtrl: NavController) {
+	announcements: Announcement[]
+	pageTitle: string = 'Announcements';
+	constructor(private navCtrl: NavController,
+			private dateService: DateProvider,
+			private announceService: AnnouncementsProvider,
+			private userService: UserProvider) {}
 
-  }
+	ionViewWillEnter(){
+		this.announceService.getAnnouncements().subscribe(anns => {
+			this.announcements = anns.map(a => {
+				const data = a.payload.doc.data() as Announcement;
+				data.timestamp = this.dateService.toDateTime(data.timestamp)
+				const id = a.payload.doc.id;
+				return { id, ...data };
+			})
+		});
+	}
 
 }
